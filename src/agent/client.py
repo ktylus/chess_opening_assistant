8f362c1cf13ebce900504ad9bdab7f5d36b98421
ktylus <karaxek23@gmail.com>
2026-06-06 15:38:43 +0200

    refactor: minor client refactor

--- src/agent/client.py
+++ src/agent/client.py
@@ -9,11 +9,6 @@ from src.chess_utils.board_state import pgn_to_fen
 
 MODEL = "gemini-3.1-flash-lite"
 
-load_dotenv()
-
-
-model = init_chat_model(model=MODEL, model_provider="google_genai")
-
 system_prompt = """
     You are a helpful assistant aiding in learning chess openings.
 """
@@ -21,21 +16,12 @@ system_prompt = """
 
 class Client:
     def __init__(self):
-        pass
-
-    @staticmethod
-    def _to_langchain_messages(chat_request: ChatRequest) -> list[BaseMessage]:
-        messages = []
-        for message in chat_request.messages:
-            if message.role == MessageRole.USER:
-                messages.append(HumanMessage(message.content))
-            elif message.role == MessageRole.ASSISTANT:
-                messages.append(AIMessage(message.content))
-        return messages
+        load_dotenv()
+        self.model = init_chat_model(model=MODEL, model_provider="google_genai")
 
     async def stream(self, chat_request: ChatRequest):
         retrieval_tool = make_fen_retrieve_tool(pgn_to_fen(chat_request.pgn))
-        agent = create_agent(model, tools=[retrieval_tool])
+        agent = create_agent(self.model, tools=[retrieval_tool])
         position_context = (
             f"\n\nCurrent position (PGN): {chat_request.pgn}"
             if chat_request.pgn
@@ -48,3 +34,13 @@ class Client:
         async for chunk in agent.astream(messages, stream_mode="messages"):  # type: ignore
             msg = chunk[0].content[0]["text"] if chunk else ""  # type: ignore
             yield msg
+
+    @staticmethod
+    def _to_langchain_messages(chat_request: ChatRequest) -> list[BaseMessage]:
+        messages = []
+        for message in chat_request.messages:
+            if message.role == MessageRole.USER:
+                messages.append(HumanMessage(message.content))
+            elif message.role == MessageRole.ASSISTANT:
+                messages.append(AIMessage(message.content))
+        return messages
