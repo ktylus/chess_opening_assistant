c32d0758159a6d275412f123f2788085aaf0b005
ktylus <karaxek23@gmail.com>
2026-06-13 18:04:43 +0200

    feat: init client only once

--- api.py
+++ api.py
@@ -5,9 +5,9 @@ from src.agent.chat_models import ChatRequest
 from src.agent.client import Client
 
 app = FastAPI()
+client = Client()
 
 
 @app.post("/chat")
 async def chat(chat_request: ChatRequest):
-    client = Client()
     return StreamingResponse(client.stream(chat_request), media_type="text/plain")
